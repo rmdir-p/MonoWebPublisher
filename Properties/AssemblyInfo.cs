bf7493de965aa6835b3d8d49dccc3c92c538e258
zji <me@z-ji.info>
2014-11-24 17:18:58 +0800

    no message

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,16 +1,15 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("MonoWebPublisher")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("MonoWebPublisher")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("MonoWebPublisher")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright ©  2014 zji")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
